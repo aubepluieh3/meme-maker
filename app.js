4ac0219991187c1feaabb03ecc64f293e722a8a6
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-08-29 18:58:32 +0900

    9. Paint Color part One

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -26,6 +27,11 @@ function onLineWidthChange(event){
     ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -34,3 +40,4 @@ canvas.addEventListener("mouseleave", cancelPainting);
 
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
